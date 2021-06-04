993ba1fdacc272084c49baa6f1345695e9d3ad8e
Kouichi Shimomura <kouichi.shimomura@pasonadigisol.jp>
2021-06-04 16:00:43 +0900

    modified:ランキングの実装完了

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 'use strict';
 const fs = require('fs');
 const readline = require('readline');
+// popu-pref.csv ファイルから、ファイルを読み込みを行う Stream（ストリーム）を生成
 const rs = fs.createReadStream('./popu-pref.csv');
+// rl というオブジェクトも Stream のインタフェースを持っている
 const rl = readline.createInterface({ input: rs, output: {} });
 const prefectureDataMap = new Map(); // key: 都道府県 value: 集計データのオブジェクト
+
+// rl オブジェクトで line というイベントが発生したら この無名関数を呼んでください、という意味
 rl.on('line', lineString => {
   const columns = lineString.split(',');
   const year = parseInt(columns[0]);
@@ -28,13 +32,15 @@ rl.on('line', lineString => {
   }
 });
 rl.on('close', () => {
+  // 変化率の計算は、その県のデータが揃 ったあとでしか正しく行えないので、 以下のように close イベントの中へ実装する
   for (const [key, value] of prefectureDataMap) {
     value.change = value.popu15 / value.popu10;
   }
+  // Array.from(prefectureDataMap) の部分で、連想配列を普通の配列に変換する処理を行っている
+  // 更に、Array の sort 関数に、比較関数として無名関数（アロー関数）を渡している
   const rankingArray = Array.from(prefectureDataMap).sort((pair1, pair2) => {
     return pair2[1].change - pair1[1].change;
-  });
-  // map関数は、 Array の要素それぞれを、与えられた関数を適用した新しい配列に変換する
+  });    
   const rankingStrings = rankingArray.map(([key, value]) => {
     return (
       key +
@@ -45,8 +51,6 @@ rl.on('close', () => {
       ' 変化率:' +
       value.change
     );
-  });  
+  });
   console.log(rankingStrings);
 });
-
-
